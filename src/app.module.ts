80ab3ea59b8c9280dc9bf75fe82beaa150715965
Hoan Nguyen <hoan.nv.bk63@gmail.com>
2022-05-21 23:39:36 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DB_HOST, DB_NAME, DB_PASS, DB_PORT } from './config';
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from './config';
+import { User } from './user/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,12 +12,13 @@ import { DB_HOST, DB_NAME, DB_PASS, DB_PORT } from './config';
       type: 'mysql',
       host: DB_HOST,
       port: DB_PORT,
-      username: DB_NAME,
+      username: DB_USER,
       password: DB_PASS,
-      database: 'test',
-      entities: [],
+      database: DB_NAME,
+      entities: [User],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const LOG_FILTER =
 export const DB_TYPE = process.env.DB_TYPE || 'mysql';
 export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
 export const DB_PORT = parseInt(process.env.DB_PORT || '3306');
-export const DB_USER = process.env.DB_USER;
-export const DB_PASS = process.env.DB_PASS;
-export const DB_NAME = process.env.DB_NAME || '';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASS = process.env.DB_PASS || 'hoan';
+export const DB_NAME = process.env.DB_NAME || 'test';
 export const DB_DEBUG = process.env.DB_DEBUG === 'true';
