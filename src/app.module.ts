82b41e49cef84f573719235175c374f9db2db335
Hoan Nguyen <hoan.nv.bk63@gmail.com>
2022-05-23 12:43:21 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,32 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisClientOptions } from 'redis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from './config';
-import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'test-nest-app-db.cluster-cgfcarg5bzgl.us-east-1.rds.amazonaws.com',
-      port: DB_PORT,
-      username: 'admin',
-      password: 'testtest',
-      database: 'testdb',
-      entities: [User],
-      synchronize: true,
-    }),
-    UserModule,
+    // ConfigModule.forRoot({ isGlobal: true }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: DB_HOST,
+    //   port: DB_PORT,
+    //   username: DB_USER,
+    //   password: DB_PASS,
+    //   database: DB_NAME,
+    //   synchronize: true,
+    //   autoLoadEntities: true,
+    // }),
+    // CacheModule.register<RedisClientOptions>({
+    //   store: redisStore,
+    //   url: 'redis://clustercfg.test-redis.p49wgc.memorydb.us-east-1.amazonaws.com:6379',
+    //   isGlobal: true,
+    // }),
+    // UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
